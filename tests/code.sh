63d614dfbea97015393ac03868977620fd5898e5
su0012004 <yx-dev@Mac.lan>
2025-08-08 18:48:09 +0800

    testy

---
+++ tests/code.sh
@@ -0,0 +1,15 @@
+# tests/code.sh
+
+# 完善当前测试用例 
+# 对应的路由 config/Routes.php  规则是： ['GET','/{code}' , 'ShortUrlController@redirect'],
+
+
+#  请求 URL   http://localhost:8000/abc123 
+# 请求方法 get 
+# 输入 短链接 
+
+# 结果  
+
+
+# 输入  docs/code.md 
+
